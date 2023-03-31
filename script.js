022d2cd295bed82aeb98ba4f98e49f4d4cef3a82
Pedro Henrique Andreatti <106180196+Pedro-devs@users.noreply.github.com>
2023-03-31 17:54:22 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+function entrevistar() {
+    var entrevista = document.getElementById("entrevista");
+    var recrutador=document.getElementById("recrutador")
+    if (recrutador.checked) {
+      entrevista.style.display = "block";
+      window.scrollTo(0, 0);
+    } else {
+      entrevista.style.display = "none";
+    }
+  }
+  
+  document.getElementById("nao-responder").addEventListener("click", function(event) {
+    event.preventDefault();
+    document.getElementById("entrevista").style.display = "none";
+  });
+  
